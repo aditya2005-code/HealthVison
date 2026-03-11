178d057456c8ffdb429cb45cd7bba85d46a493c5
Aditya <2k23.csds2312133@gmail.com>
2026-03-11 19:47:13 +0530

    set python version to 3.11

--- ml-service/api/services/ocr_service.py
+++ ml-service/api/services/ocr_service.py
@@ -7,7 +7,7 @@ import numpy as np
 
 import pytesseract
 
-ss# -----------------------------
+# -----------------------------
 # Try extracting text directly
 # -----------------------------
 def extract_text_from_pdf(pdf_path):
