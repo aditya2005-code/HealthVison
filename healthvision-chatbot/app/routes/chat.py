f5d03fdcce7fd9ad90721b8c37dd238380d1d687
Aditya <2k23.csds2312133@gmail.com>
2026-03-12 22:23:47 +0530

    feat(routes/chat) Add route for the general chat response and validated it with postman

--- healthvision-chatbot/app/routes/chat.py
+++ healthvision-chatbot/app/routes/chat.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from app.models.chat_model import ChatRequest , ChatResponse
-from app.utils.prompt_builder import build_chat_prompt
+from app.models.chat_model import ChatRequest , ChatResponse , GeneralChatRequest
+from app.utils.prompt_builder import build_chat_prompt, build_general_prompt
 from app.services.llm_service import generate_chat_response
 
 
@@ -12,5 +12,13 @@ def chat_with_bot(request:ChatRequest):
     prompt = build_chat_prompt(request.analysis , request.question)
 
     answer = generate_chat_response(prompt)
+
+    return ChatResponse(answer=answer)
+
+@router.post("/general-chat" , response_model=ChatResponse)
+def general_chat(request : GeneralChatRequest):
+
+    prompt =  build_general_prompt(request.question)
+    answer  =  generate_chat_response(prompt)
 
     return ChatResponse(answer=answer)
