7d3a853aab8adc80cf00d19abd507ae00346c4d6
Aditya <2k23.csds2312133@gmail.com>
2026-03-12 16:10:43 +0530

    feat(routes/chat.py) Integrated route for the chatbot and validated through postman:

--- healthvision-chatbot/app/routes/chat.py
+++ healthvision-chatbot/app/routes/chat.py
@@ -1,11 +1,16 @@
 from fastapi import APIRouter
 from app.models.chat_model import ChatRequest , ChatResponse
+from app.utils.prompt_builder import build_chat_prompt
+from app.services.llm_service import generate_chat_response
+
 
 router = APIRouter()
 
 @router.post("/chat" , response_model=ChatResponse)
 def chat_with_bot(request:ChatRequest):
 
-    return ChatResponse(
-        answer="Chatbot is working"
-    )
+    prompt = build_chat_prompt(request.analysis , request.question)
+
+    answer = generate_chat_response(prompt)
+
+    return ChatResponse(answer=answer)
